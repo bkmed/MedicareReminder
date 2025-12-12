4045ec139a2c684531bcb9ce304e9dc4f3af4083
mohamed ben khedher <ben.khedhermohamed0@gmail.com>
2025-12-12 18:02:26 +0100

    fix github deploy

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, './build'),
     filename: 'index_bundle.js',
-    publicPath: '/',
+    publicPath: 'auto',
     clean: true,
   },
   devServer: {
