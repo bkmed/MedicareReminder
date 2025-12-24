68a972f5b04b71e84384a8764f90cd8ba0915227
mohamed ben khedher <ben.khedhermohamed0@gmail.com>
2025-12-24 17:17:32 +0100

    clean

--- index.web.js
+++ index.web.js
@@ -2,7 +2,6 @@ import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-// Register the app
 AppRegistry.registerComponent(appName, () => App);
 
 const rootTag =
